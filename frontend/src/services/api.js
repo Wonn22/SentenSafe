4d8c0d1135a4524d603b13d35f6ac0ee2c2c887a
Wonn22 <juanwilbert220906@gmail.com>
2026-05-22 09:46:22 +0700

    Use environment backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000/analyze'
+const API_URL = `${import.meta.env.VITE_API_URL}/analyze`
 
 export async function analyzeText(text) {
   const response = await fetch(API_URL, {
